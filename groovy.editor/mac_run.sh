dcc4a8e87ed7f610015a6e36a923ec79a52c33e6
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2009-03-27 17:11:35 +0000

    Simple run script.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@41 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

---
+++ groovy.editor/mac_run.sh
@@ -0,0 +1,2 @@
+java -cp /opt/local/share/java/groovy/embeddable/groovy-all-1.5.7.jar:/Users/seldred/workspaces/dev/org.eclipse.swt/swt.jar:/Users/seldred/workspaces/dev/editor/target/editor-1.0.jar:/Users/seldred/workspaces/dev/editor/src/test/resources/:./bin-groovy/ GRpgEditor -XstartOnFirstThread
+
