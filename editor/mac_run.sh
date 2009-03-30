eb4008bc2918a7c1d3b21a9f76811ef70004648f
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2009-03-30 09:21:01 +0000

    Better run script.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@45 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- editor/mac_run.sh
+++ editor/mac_run.sh
@@ -1,1 +1,2 @@
-java -cp /Users/seldred/workspaces/dev/org.eclipse.swt/swt.jar:./target/editor-1.0.jar:./src/test/resources/ rpg.editor.RpgEditor -XstartOnFirstThread
+SWT_JAR="$HOME/.m2/repository/org/eclipse/swt/carbon/macosx/3.4.0/macosx-3.4.0.jar"
+java -cp $SWT_JAR:./target/editor-1.0.jar:./src/test/resources/ rpg.editor.RpgEditor -XstartOnFirstThread
