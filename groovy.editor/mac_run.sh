0bc8d5e31b71744297ac7a261b8572642e74e073
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2009-03-30 09:19:30 +0000

    Better run script.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@44 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- groovy.editor/mac_run.sh
+++ groovy.editor/mac_run.sh
@@ -1,2 +1,13 @@
-java -cp /opt/local/share/java/groovy/embeddable/groovy-all-1.5.7.jar:/Users/seldred/workspaces/dev/org.eclipse.swt/swt.jar:/Users/seldred/workspaces/dev/editor/target/editor-1.0.jar:/Users/seldred/workspaces/dev/editor/src/test/resources/:./bin-groovy/ GRpgEditor -XstartOnFirstThread
+# compile everything to java classes
 
+SWT_JAR="$HOME/.m2/repository/org/eclipse/swt/carbon/macosx/3.4.0/macosx-3.4.0.jar"
+EDITOR_JAR="$HOME/workspaces/dev/editor/target/editor-1.0.jar"
+
+groovyc -classpath $SWT_JAR:$EDITOR_JAR:./bin-groovy src/*.groovy -d bin-groovy
+
+# run as java
+
+GROOVY_JAR="$HOME/Java/groovy-1.5.6/embeddable/groovy-all-1.5.6.jar"
+RESOURCES="$HOME/workspaces/dev/editor/src/test/resources/"
+
+java -cp $GROOVY_JAR:$SWT_JAR:$EDITOR_JAR:$RESOURCES:./bin-groovy GRpgEditor -XstartOnFirstThread
