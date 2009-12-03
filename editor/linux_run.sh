e3339741dd54925a43b3691504ced4dc569e6858
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2009-12-03 16:58:28 +0000

    Moved to gradle.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@93 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- editor/linux_run.sh
+++ editor/linux_run.sh
@@ -1,2 +1,1 @@
-SWT_JAR="$HOME/.m2/repository/org/eclipse/swt/carbon/gtk/3.4.0/gtk-3.4.0.jar"
-java -cp $SWT_JAR:./target/editor-1.0.jar:./src/test/resources/ rpg.editor.RpgEditor -XstartOnFirstThread
+java -cp ./target/editor-1.0.jar:./lib/swt-linux-gtk-3.4.1.jar:./src/test/resources/ rpg.editor.RpgEditor -XstartOnFirstThread
