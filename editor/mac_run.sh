c0233b37503c32927b1acbc758eee6724a1d9fb6
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2010-05-27 15:25:59 +0000

    Removed SWT 3.5.2.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@104 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- editor/mac_run.sh
+++ editor/mac_run.sh
@@ -1,1 +1,1 @@
-java -cp ./build/libs/editor-1.0.jar:./lib/swt-macosx-cocoa-x86-64-3.5.2.jar:./src/test/resources/ rpg.editor.RpgEditor -XstartOnFirstThread
+java -cp ./build/libs/editor-1.0.jar:./lib/swt-macosx-cocoa-x86-64-3.5.2.jar:./src/test/resources/ rpg.editor.RpgEditor -XstartOnFirstThread -d32
