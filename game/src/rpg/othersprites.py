bfe8f1d8c4ec6fdc58dd076050e4e4d64cfa64ed
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2011-05-23 11:40:09 +0000

    Beetle images added.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@122 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- game/src/rpg/othersprites.py
+++ game/src/rpg/othersprites.py
@@ -7,15 +7,15 @@ class Baddie(OtherSprite):
     
     framesImage = None
     
-    baseRectSize = (4 * SCALAR, 7 * SCALAR)    
+    baseRectSize = (4 * SCALAR, 12 * SCALAR)    
 
     def __init__(self, rpgMap):
         if Baddie.framesImage is None:    
-            imagePath = os.path.join(SPRITES_FOLDER, "flame-frames.png")
+            imagePath = os.path.join(SPRITES_FOLDER, "beetle-frames.png")
             Baddie.framesImage = view.loadScaledImage(imagePath, None)        
         animationFrames = view.processStaticFrames(Baddie.framesImage)
-        spriteFrames = StaticFrames(animationFrames, 6)
-        OtherSprite.__init__(self, rpgMap, spriteFrames, (4, 2))
+        spriteFrames = StaticFrames(animationFrames, 12)
+        OtherSprite.__init__(self, rpgMap, spriteFrames)
 
     def processCollision(self, player):
         print "life lost!"
