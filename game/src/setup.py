9b15ad30d7f45cf2881e57de07056bd2fec92bf3
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2011-08-31 08:26:36 +0000

    Removed data files
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@160 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- game/src/setup.py
+++ game/src/setup.py
@@ -10,10 +10,3 @@ setup(name = "rpg-world",
       author_email="samuel.eldred@googlemail.com",
       url="http://code.google.com/p/rpg-world/"
 )
-
-"""
-      data_files=[("maps", ["maps/caves.map", "maps/central.map", "maps/east.map"]),
-                  ("sounds", ["sounds/door.wav", "sounds/lifelost.wav", "sounds/pickup.wav", "sounds/wasp.wav"]),
-                  ("tiles", ["tiles/dungeon.png", "tiles/dungeon_metadata.txt", "tiles/earth.png", "tiles/earth_metadata.txt", "tiles/grass.png", "tiles/grass_metadata.txt", "tiles/objects.png", "tiles/objects_metadata.txt", "tiles/water.png", "tiles/water_metadata.txt", "tiles/wood.png", "tiles/wood_metadata.txt"]),
-                  ("sprites", ["sprites/beetle-frames.png", "sprites/chest.png", "sprites/coin-frames.png", "sprites/door-frames.png", "sprites/flame-frames.png", "sprites/font.png", "sprites/key-frames.png", "sprites/life.png", "sprites/numbers.png", "sprites/rock.png", "sprites/small-coin.png", "sprites/small-key.png", "sprites/ulmo-frames.png", "sprites/wasp-frames.png"])]
-"""
