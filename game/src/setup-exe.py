7d4747dc7dfe97e593093660a87d7397a331b92e
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2011-10-06 20:15:28 +0000

    Added files to setup-exe.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@178 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- game/src/setup-exe.py
+++ game/src/setup-exe.py
@@ -7,13 +7,13 @@ import py2exe
 setup(windows = ["play.py"],
       # console = ["play.py"],
       name = "rpg-world",
-      version = "0.9",
+      version = "0.91",
       packages=["rpg"],
       author="Sam Eldred",
       author_email="samuel.eldred@googlemail.com",
       url="http://code.google.com/p/rpg-world/",
-      data_files=[("maps", ["maps/caves.map", "maps/central.map", "maps/east.map"]),
-                  ("sounds", ["sounds/door.wav", "sounds/lifelost.wav", "sounds/pickup.wav", "sounds/wasp.wav"]),
+      data_files=[("maps", ["maps/caves.map", "maps/central.map", "maps/east.map", "maps/northcave.map"]),
+                  ("sounds", ["sounds/door.wav", "sounds/footstep.wav", "sounds/lifelost.wav", "sounds/pickup.wav", "sounds/swoosh.wav", "sounds/wasp.wav", "sounds/beetle.wav", "sounds/endgame.wav"]),
                   ("tiles", ["tiles/dungeon.png", "tiles/dungeon_metadata.txt", "tiles/earth.png", "tiles/earth_metadata.txt", "tiles/grass.png", "tiles/grass_metadata.txt", "tiles/objects.png", "tiles/objects_metadata.txt", "tiles/water.png", "tiles/water_metadata.txt", "tiles/wood.png", "tiles/wood_metadata.txt"]),
                   ("sprites", ["sprites/beetle-frames.png", "sprites/chest.png", "sprites/coin-frames.png", "sprites/door-frames.png", "sprites/flame-frames.png", "sprites/font.png", "sprites/key-frames.png", "sprites/life.png", "sprites/numbers.png", "sprites/rock.png", "sprites/small-coin.png", "sprites/small-key.png", "sprites/ulmo-frames.png", "sprites/wasp-frames.png"])]
 )
