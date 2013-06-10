160e4bc07e7e899005c0ccfa3eab3f790018e01c
Richard Hull <rm_hull@yahoo.co.uk>
2013-06-10 23:56:39 +0100

    Added wiimote support

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ What's New
 ----------
 * (10/06/2013) Forked from svn into git
 * (10/06/2013) Use a Wii-mote to control Ulmo
+* (10/06/2013) Optimized screen for QVGA LCD
 
 Now optimised for the Raspberry Pi!
 -----------------------------------
@@ -24,3 +25,23 @@ implemented in Python/Pygame and the map editor is in Java SWT.
 See also http://pygame.org/project-Ulmo%27s+Adventure-2042-3702.html
 
 ![Screenshot](https://raw.github.com/rm-hull/ulmos-adventure/master/2042.png)
+
+Using the Wiimote
+-----------------
+Ensure that the pythonr-cwiid module is installed and bluetooth is
+installed and enabled (apt package 'bluez').
+
+    $ dmesg | grep -i blue
+    [    5.819514] Bluetooth: Core ver 2.16
+    [    5.824335] Bluetooth: HCI device and connection manager initialized
+    [    5.824407] Bluetooth: HCI socket layer initialized
+    [    5.824451] Bluetooth: L2CAP socket layer initialized
+    [    5.824536] Bluetooth: SCO socket layer initialized
+    [   30.849159] Bluetooth: BNEP (Ethernet Emulation) ver 1.3
+    [   30.849192] Bluetooth: BNEP filters: protocol multicast
+    [   30.849239] Bluetooth: BNEP socket layer initialized
+    [   30.872984] Bluetooth: RFCOMM TTY layer initialized
+    [   30.873067] Bluetooth: RFCOMM socket layer initialized
+    [   30.873084] Bluetooth: RFCOMM ver 1.11
+
+    
--- game/src/play.py
+++ game/src/play.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 
-from pygame.locals import KEYDOWN, K_ESCAPE, K_x, QUIT
-
+from pygame.locals import KEYDOWN, K_ESCAPE, K_x, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_SPACE, QUIT
+import time
 import pygame
+import cwiid
 
 """
 If using an older Raspberry Pi distro, you might need to run the following commands to get working sound:
@@ -17,11 +18,39 @@ pygame.init()
 
 import rpg.states
 
+def initWiimote():
+    try:
+        print "Press 1+2 on the Wii-mote to connect"
+        wii = cwiid.Wiimote()
+        wii.rpt_mode = cwiid.RPT_BTN
+        print "Detected controller:", wii
+        wii.rumble = 1
+        time.sleep(1)
+        wii.rumble = 0
+        return wii;
+    except RuntimeError:
+        print "No wiimote found, continuing without..."
+
+def getPressed():
+    if wii:
+        buttons = wii.state['buttons']
+        return {
+            K_RIGHT: (buttons & cwiid.BTN_RIGHT),
+            K_LEFT:  (buttons & cwiid.BTN_LEFT),
+            K_UP:    (buttons & cwiid.BTN_UP),
+            K_DOWN:  (buttons & cwiid.BTN_DOWN),
+            K_SPACE: (buttons & cwiid.BTN_A)
+        }
+    else:
+        return pygame.key.get_pressed()
+
+    return keyPresses
+
 def playMain():
     # get the first state
     currentState = rpg.states.showTitle()
     # start the main loop
-    clock = pygame.time.Clock()    
+    clock = pygame.time.Clock()
     while True:
         clock.tick(rpg.states.FRAMES_PER_SEC)
         for event in pygame.event.get():
@@ -30,8 +59,8 @@ def playMain():
             if event.type == KEYDOWN and event.key == K_x:
                 # mute sound handler
                 rpg.states.soundHandler.toggleSound()
-        # detect key presses    
-        keyPresses = pygame.key.get_pressed()
+        # detect key presses
+        keyPresses = getPressed()
         # delegate key presses to the current state
         newState = currentState.execute(keyPresses)
         # flush sounds
@@ -41,4 +70,6 @@ def playMain():
             currentState = newState
 
 # this calls the testMain function when this script is executed
-if __name__ == '__main__': playMain()
+if __name__ == '__main__':
+    wii = initWiimote()
+    playMain()
