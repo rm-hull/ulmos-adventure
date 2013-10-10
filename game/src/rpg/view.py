2311aabe6814fbd34eca919b0e90feaccc954f9f
Richard Hull <rm_hull@yahoo.co.uk>
2013-10-10 15:39:40 +0100

    Revert "CRLF?"
    This reverts commit d101845077d1a6926e90046a47acae82d9f68792.

--- game/src/rpg/view.py
+++ game/src/rpg/view.py
@@ -26,24 +26,24 @@ DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
 def createRectangle(dimensions, colour = None):
     rectangle = pygame.Surface(dimensions).convert()
     if colour is not None:
-        rectangle.fill(colour)
+	rectangle.fill(colour)
     return rectangle
 
 def loadImage(imagePath, colourKey = None):
     try:
-        image = pygame.image.load(imagePath)
+	image = pygame.image.load(imagePath)
     except pygame.error, message:
-        print "Cannot load image: ", os.path.abspath(imagePath)
-        raise SystemExit, message
+	print "Cannot load image: ", os.path.abspath(imagePath)
+	raise SystemExit, message
     image = image.convert()
     if colourKey is not None:
-        image.set_colorkey(colourKey, RLEACCEL)
+	image.set_colorkey(colourKey, RLEACCEL)
     return image
 
 def loadScaledImage(imagePath, colourKey = None, scalar = SCALAR):
     img = loadImage(imagePath, colourKey)
     return scale(img, (img.get_width() * scalar, img.get_height() * scalar))
-        
+
 def createDuplicateSpriteImage(spriteImage):
     # transparency is set on the duplicate - this allows us to draw over
     # the duplicate image with areas that are actually transparent
@@ -62,13 +62,13 @@ def processMovementFrames(framesImage, numFrames = 4):
     animationFrames = {}
     row = 0
     for direction in DIRECTIONS:
-        frames = []
-        rowOffsetY = row * height
-        for i in range(numFrames):
-            img = framesImage.subsurface(i * width, rowOffsetY, width, height)
-            frames.append(img)
-        animationFrames[direction] = frames
-        row += 1
+	frames = []
+	rowOffsetY = row * height
+	for i in range(numFrames):
+	    img = framesImage.subsurface(i * width, rowOffsetY, width, height)
+	    frames.append(img)
+	animationFrames[direction] = frames
+	row += 1
     return animationFrames
 
 # create a copy of the given animation frames
@@ -76,11 +76,11 @@ def copyMovementFrames(animationFrames):
     # map of image lists for animation keyed on direction
     animationFramesCopy = {}
     for direction in DIRECTIONS:
-        framesCopy = []
-        for frame in animationFrames[direction]:
-            img = createDuplicateSpriteImage(frame)
-            framesCopy.append(img)
-        animationFramesCopy[direction] = framesCopy
+	framesCopy = []
+	for frame in animationFrames[direction]:
+	    img = createDuplicateSpriteImage(frame)
+	    framesCopy.append(img)
+	animationFramesCopy[direction] = framesCopy
     return animationFramesCopy
 
 # process animation frames from the composite image
@@ -91,8 +91,8 @@ def processStaticFrames(framesImage, numFrames = 4):
     # map of images for animation
     animationFrames = []
     for i in range(numFrames):
-        img = framesImage.subsurface((i * width, 0), (width, height))
-        animationFrames.append(img)
+	img = framesImage.subsurface((i * width, 0), (width, height))
+	animationFrames.append(img)
     return animationFrames
 
 # create a copy of the given animation frames
@@ -100,8 +100,8 @@ def copyStaticFrames(animationFrames):
     # map of image lists for animation keyed on direction
     animationFramesCopy = []
     for frame in animationFrames:
-        img = createDuplicateSpriteImage(frame)
-        animationFramesCopy.append(img)
+	img = createDuplicateSpriteImage(frame)
+	animationFramesCopy.append(img)
     return animationFramesCopy
 
 def createTransparentRect(dimensions):
@@ -113,9 +113,9 @@ def processFontImage(fontImage, charWidth, rows = 1):
     charImages = []
     charHeight = fontImage.get_height() // rows
     for i in range(rows):
-        x, y = 0, i * charHeight
-        while x < fontImage.get_width():
-            charImage = fontImage.subsurface((x, y), (charWidth, charHeight))
-            charImages.append(charImage)
-            x += charWidth
+	x, y = 0, i * charHeight
+	while x < fontImage.get_width():
+	    charImage = fontImage.subsurface((x, y), (charWidth, charHeight))
+	    charImages.append(charImage)
+	    x += charWidth
     return charImages
