737e0332434a41713f1bf99d42a8ad33547717c6
samuel.eldred <samuel.eldred@b611d880-fc3a-11dd-a7be-4b3ef3b0700c>
2013-11-25 22:10:20 +0000

    Enhanced package info.
    git-svn-id: http://rpg-world.googlecode.com/svn/trunk@230 b611d880-fc3a-11dd-a7be-4b3ef3b0700c

--- game/src/setup.py
+++ game/src/setup.py
@@ -8,5 +8,11 @@ setup(name = "rpg-world",
       packages=["rpg"],
       author="Sam Eldred",
       author_email="samuel.eldred@gmail.com",
-      url="http://code.google.com/p/rpg-world/"
+      url="http://code.google.com/p/rpg-world/",
+      license="GNU GPL v3",
+      description="Game/game engine written in Python/Pygame",
+      long_description="""16-bit style graphics, a top-down perspective and tile-based maps - a
+          retro style RPG much like the old SNES classics. Also features some pseudo-3D elements,
+          eg. the ability to move underneath bridges, etc.""",
+      platforms="Python source code"
 )
